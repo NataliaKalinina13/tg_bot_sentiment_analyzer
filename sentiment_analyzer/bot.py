4ef990a48620b14204f51e0370bf10bfb39e6b38
NataliaKalinina13 <kaviorneli@gmail.com>
2023-06-20 21:10:07 +0300

    .

--- sentiment_analyzer/bot.py
+++ sentiment_analyzer/bot.py
@@ -1,62 +1,77 @@
 import telebot
-from telebot import types
+import string
+import fasttext
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
-import fasttext
-
-fasttext.FastText.eprint = lambda x: None
-tokenizer = RegexTokenizer()
-FastTextSocialNetworkModel.MODEL_PATH = r'/Users/nataliakalinina/PycharmProjects/sentiment_analyzer/fasttext-social-network-model.bin'
-model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 
 
-API_token = '6185694736:AAEWztOGTsDfKzgJQHivKZ-ipKZbdbmfFAc'
+class SentimentAnalyzer():
+    def __init__(self, api_token, model_path):
+        self.tokenizer = RegexTokenizer()
+        self.model = FastTextSocialNetworkModel(tokenizer=self.tokenizer)
+        self.API_token = api_token
+        self.bot = telebot.TeleBot(API_token)
+        fasttext.FastText.eprint = lambda x: None
+        FastTextSocialNetworkModel.MODEL_PATH = model_path
 
-bot = telebot.TeleBot(API_token)
+        @self.bot.message_handler(commands=['start'])
+        def start(self, message):
+            '''output a greeting after /start'''
+            message_with_user_name = f'Привет, <b>{message.from_user.first_name}</b>. Я бот для анализа настроения твоего сообщения. Отправь мне любой текст'
+            self.bot.send_message(message.chat.id, message_with_user_name, parse_mode='html')
+
+        @self.bot.message_handler()
+        def analyze_message(self, message):
+            ''''call the preprocessing function and conduct a sentiment analysis'''
+            preprocessed_message = self.preprocessing(message)
+
+            message_tokens = self.tokenizer.split(preprocessed_message)
+            results = self.model.predict([str(message_tokens)])
+
+            positive = 0
+            negative = 0
+            neutral = 0
+
+            response = ''
+
+            for result in results:
+                try:
+                    positive += result['positive']
+                except KeyError:
+                    pass
+
+                try:
+                    negative += result['negative']
+                except KeyError:
+                    pass
+
+                try:
+                    neutral += result['neutral']
+                except KeyError:
+                    pass
+
+            if positive > negative and positive > neutral:
+                response = 'Ваше сообщение позитивное'
+            elif negative > positive and negative > neutral:
+                response = 'Ваше сообщение негативное'
+            elif neutral > positive and neutral > negative:
+                response = 'Ваше сообщение нейтральное'
+
+            self.bot.reply_to(message, response)
+
+    def preprocessing(self, message):
+        '''lower text and clean punctuation'''
+        low_message = message.text.lower()
+        clean_punct_mess = low_message.translate(str.maketrans("", "", string.punctuation))
+        return clean_punct_mess
+
+    def run_bot(self):
+        '''launch a chatbot'''
+        self.bot.polling(none_stop=True)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    message_with_user_name = f'Привет, <b>{message.from_user.first_name}</b>. Я бот для анализа настроения твоего сообщения. Отправь мне любой текст'
-    bot.send_message(message.chat.id, message_with_user_name, parse_mode='html')
-
-
-@bot.message_handler()
-def analyze_message(message):
-    message_text = message.text
-
-    results = model.predict([message_text])
-
-    positive = 0
-    negative = 0
-    neutral = 0
-
-    response = ''
-
-    for result in results:
-        try:
-            positive += result['positive']
-        except KeyError:
-            pass
-
-        try:
-            negative += result['negative']
-        except KeyError:
-            pass
-
-        try:
-            neutral += result['neutral']
-        except KeyError:
-            pass
-
-    if round(positive) > 0 and round(negative) == 0 and round(neutral) == 0:
-        response = 'Ваше сообщение позитивное'
-    elif round(positive) == 0 and round(negative) > 0 and round(neutral) == 0:
-        response = 'Ваше сообщение негативное'
-    elif round(positive) == 0 and round(negative) == 0 and round(neutral) > 0:
-        response = 'Ваше сообщение нейтральное'
-
-    bot.reply_to(message, response)
-
-
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    API_token = '6185694736:AAEWztOGTsDfKzgJQHivKZ-ipKZbdbmfFAc'
+    model_path = r'/Users/nataliakalinina/PycharmProjects/sentiment_analyzer/fasttext-social-network-model.bin'
+    bot = SentimentAnalyzer(API_token, model_path)
+    bot.run_bot()
